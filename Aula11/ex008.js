147e8ccf6a5bba426889d3b538eb1e29b96fd5af
Jeiel limeira <jeiel.anteroz@hotmail.com>
2024-11-03 12:43:25 -0300

    EX008

---
+++ Aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel = 12.5
+console.log(`A velocidade do seu carro ${vel} `)
+console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+console.log(`Dirija sempre usando cinto de segurança `)
