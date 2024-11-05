837fd0f7ce80899146120651babbd798a9def8bb
Jeiel limeira <jeiel.anteroz@hotmail.com>
2024-11-04 22:38:14 -0300

    Update ex011.js

--- Aula12/ex011.js
+++ Aula12/ex011.js
@@ -1,7 +1,8 @@
-var idade = 22
+var idade = 66
+console.log(`você tem ${idade} anos`)
 if (idade < 16){
     console.log('não vota')
-} else if (idade < 18){
+} else if (idade < 18 || idade > 65){
     console.log('voto opcional')
    } else {
     console.log('voto obrigatorio')
