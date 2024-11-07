bb002e05a814c0d6cf62f6a625498e1a0aa94ef7
Jeiel limeira <jeiel.anteroz@hotmail.com>
2024-11-06 21:33:06 -0300

    sao paulino bixa

---
+++ Aula12/ex013.js
@@ -0,0 +1,7 @@
+var bixa = 'sao paulino'
+console.log('veja se você é bixa pelo time de futebol')
+if(bixa == 'sao paulino'){
+    console.log('Tu é bixa demais')
+} else{
+    console.log('tercedor homem maximo respeito')
+}
