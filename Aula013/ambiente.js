d1ec4b813d71bebf4faa5c795f7e6e235d0c8a68
Jeiel limeira <jeiel.anteroz@hotmail.com>
2024-11-16 13:56:03 -0300

    Create ambiente.js

---
+++ Aula013/ambiente.js
@@ -0,0 +1,22 @@
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
+
+var c = 1
+while(c < 51){
+    console.log(`${c}`)
+    c++
+}
+
+var c =1
+do{(c <= 51)
+    console.log(`${c}`)
+    c++
+} while(c <= 6)
